7998bee0c7a64ff7d1da485f0363b4506f571fb8
Kate Roshchupkina <katerina.ermakov@mail.ru>
2023-09-11 14:43:47 +0300

    sprint 12

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,8 +2,10 @@ import logo from "../images/header__logo.svg";
 import menu from "../images/menu.svg";
 import closeIcon from "../images/close_mobile.svg";
 import { useState } from "react";
+import { Route, Routes, Link } from "react-router-dom";
+import * as token from "../utils/token";
 
-function Header() {
+function Header({ userData }) {
   const [isMenu, setIsMenu] = useState(true);
 
   function handleClickMenu() {
@@ -14,15 +16,50 @@ function Header() {
     setIsMenu(!isMenu);
   }
 
+  function handleRemoveToken() {
+    token.removeToken();
+    localStorage.setItem("loggedIn", JSON.stringify(false));
+  }
+
   return (
     <>
       <header className='header'>
-        <p className='header__text'>
-          email@mail.com
-          <a className='header__exit' href='#'>
-            Выйти
-          </a>
-        </p>
+        <Routes>
+          <Route
+            path='signup'
+            element={
+              <Link to='/signin' className='header__link'>
+                Войти
+              </Link>
+            }
+          />
+          <Route
+            path='signin'
+            element={
+              <Link to='/signup' className='header__link'>
+                Регистрация
+              </Link>
+            }
+          />
+          <Route
+            path='/'
+            element={
+              <>
+                <p className='header__text'>
+                  {userData?.email || ""}
+                  <Link
+                    to='/signin'
+                    className='header__exit'
+                    onClick={handleRemoveToken}
+                  >
+                    Выйти
+                  </Link>
+                </p>
+              </>
+            }
+          />
+        </Routes>
+
         <div className='header__main'>
           <img className='header__logo' src={logo} alt='Логотип' />
 
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,10 +2,10 @@ import { Outlet } from "react-router-dom";
 import Header from "./Header";
 import Footer from "./Footer";
 
-const Layout = () => {
+const Layout = ({ userData }) => {
   return (
     <>
-      <Header />
+      <Header userData={userData} />
 
       <Outlet />
       <Footer />
